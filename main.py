dd863f9f3828e1faff520620ca4788d07c4a871e
Pederzh <luigipederzani@gmail.com>
2021-09-09 16:17:12 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+
+response = requests.post('https://services.sentinel-hub.com/api/v1/process',
+  headers={"Authorization" : "Bearer <your_access_token>"},
+  json={
+    "input": {
+        "bounds": {
+            "bbox": [
+                13.822174072265625,
+                45.85080395917834,
+                14.55963134765625,
+                46.29191774991382
+            ]
+        },
+        "data": [{
+            "type": "sentinel-2-l2a"
+        }]
+    },
+    "evalscript": """
+    //VERSION=3
+
+    function setup() {
+      return {
+        input: ["B02", "B03", "B04"],
+        output: {
+          bands: 3
+        }
+      };
+    }
+
+    function evaluatePixel(
+      sample,
+      scenes,
+      inputMetadata,
+      customData,
+      outputMetadata
+    ) {
+      return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+    }
+    """
+})
