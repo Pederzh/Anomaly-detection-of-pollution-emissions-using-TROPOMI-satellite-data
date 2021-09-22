6bdb84a75d8a650591aa07fd23f709071fb4fa62
Unknown <Frao.fiori1997@gmail.com>
2021-09-22 21:36:22 +0200

    worked on statistics visualization

--- stats_manager.py
+++ stats_manager.py
@@ -2,6 +2,7 @@ import datetime
 import io
 import json
 import math
+import statistics as stst
 
 import matplotlib.pyplot as plt
 
@@ -58,13 +59,6 @@ def image_to_sorted_list(image):
     list.sort()
     return list
 
-
-def image_median_value(sorted_list):
-    if len(sorted_list) == 0: return -1
-    median_position = int((len(sorted_list) - 1) / 2)
-    return sorted_list[median_position]
-
-
 def image_mode_value(frequencies):
     keys = list(frequencies.keys())
     max_freq = 0
@@ -75,26 +69,40 @@ def image_mode_value(frequencies):
             mode = int(keys[i])
     return mode
 
-
-def image_max_value(sorted_list):
+def list_max_value(sorted_list):
     if len(sorted_list) == 0: return -1
     return sorted_list[len(sorted_list) - 1]
 
 
-def image_min_value(sorted_list):
+def list_min_value(sorted_list):
     if len(sorted_list) == 0: return -1
     return sorted_list[0]
 
 
-def image_min_quartile_value(sorted_list):
+def list_min_quartile_value(sorted_list):
     if len(sorted_list) == 0: return -1
-    return sorted_list[int((len(sorted_list) - 1) / 4)]
+    return sorted_list[int(round(((len(sorted_list) - 1) / 4), 0))]
 
-
-def image_max_quartile_value(sorted_list):
+def list_max_quartile_value(sorted_list):
     if len(sorted_list) == 0: return -1
-    return sorted_list[int((len(sorted_list) - 1) * 3 / 4)]
+    return sorted_list[int(round(((len(sorted_list) - 1) * 3 / 4), 0))]
 
+def list_mean_value(sorted_list):
+    if len(sorted_list) == 0: return -1
+    return stst.mean(sorted_list)
+
+def list_mode_value(sorted_list):
+    if len(sorted_list) == 0: return -1
+    return stst.mode(sorted_list)
+
+def list_median_value(sorted_list):
+    if len(sorted_list) == 0: return -1
+    return stst.median(sorted_list)
+
+def list_variance_value(sorted_list, mean):
+    if len(sorted_list) == 0: return -1
+    if len(sorted_list) == 1: return 0
+    return stst.variance(sorted_list, mean)
 
 def image_to_frequencies(sorted_list):
     values = {}
@@ -150,10 +158,11 @@ def images_variation_variance(image, next, mean):
     return -1
 
 
+
 def get_image_stats(image):
     sorted_list = image_to_sorted_list(image)
     frequencies = image_to_frequencies(sorted_list)
-    median = image_median_value(sorted_list)
+    median = list_median_value(sorted_list)
     mode = image_mode_value(frequencies)
     mean = image_mean_value(image)
     stats = {
@@ -168,11 +177,11 @@ def get_image_stats(image):
             "zeroes_frequency": image_non_zero_values_ratio(image),
         },
         "box_plot": {
-            "min": image_min_value(sorted_list),
-            "quartile_025": image_min_quartile_value(sorted_list),
+            "min": list_min_value(sorted_list),
+            "quartile_025": list_min_quartile_value(sorted_list),
             "median": median,
-            "quartile_075": image_max_quartile_value(sorted_list),
-            "max": image_max_value(sorted_list),
+            "quartile_075": list_max_quartile_value(sorted_list),
+            "max": list_max_value(sorted_list),
         },
     }
     return stats
@@ -187,6 +196,94 @@ def get_image_stats_and_variation(image, image_next):
     }
     return stats
 
+def date_to_str(date):
+    date_str = str(date.year) + "-"
+    if (date.month < 10): date_str += "0"
+    date_str += str(date.month) + "-"
+    if (date.day < 10): date_str += "0"
+    date_str += str(date.day)
+    return date_str
+
+
+
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#                   DAYS STATS
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+def save_days_stats(data, directory_path):
+    stats = {}
+    keys = list(data.keys())
+    for i in range(len(keys)):
+        if i != 0:
+            stat = get_image_stats_and_variation(data[keys[i]], data[keys[i - 1]])
+        else:
+            stat = get_image_stats(data[keys[i]])
+        stats[keys[i]] = stat
+        if i % 100 == 0: print(i)
+    with open(directory_path + "Statistics/" + "days.json", 'w') as outfile:
+        json.dump(stats, outfile)
+
+
+
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#                   PERIOD MATRIX
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+def save_period_stats(data_set, date_start, date_end, location_name, product_type):
+    pixels = []
+    for y in range(len(data_set[date_to_str(date_start)])):
+        pixels.append([])
+        for x in range(len(data_set[date_to_str(date_start)][y])):
+            list = []
+            # generating pixel list of different days
+            for i in range(int((date_end - date_start).days)):
+                date = date_start + datetime.timedelta(days=i)
+                if data_set[date_to_str(date)][y][x] != -1:
+                    list.append(data_set[date_to_str(date)][y][x])
+            list.sort()
+            mean = list_mean_value(list)
+            pixel = {
+                "mean": mean,
+                "median": list_median_value(list),
+                "mode": list_mode_value(list),
+                "variance": list_variance_value(list, mean),
+                "min": list_min_value(list),
+                "min_quartile": list_min_quartile_value(list),
+                "max_quartile": list_max_quartile_value(list),
+                "max": list_max_value(list),
+                "zeroes": int((date_end - date_start).days) - len(list),
+                "non_zeroes": len(list)
+            }
+            pixels[y].append(pixel)
+
+    stats = {
+        "info": {
+            "location_name": location_name,
+            "product_type": product_type,
+            "date_start": date_to_str(date_start),
+            "date_end": date_to_str(date_end),
+        },
+        "data": pixels
+    }
+    # saving obj pixels as json file
+    info_name = location_name.strip() + "_" + product_type + "_"
+    file_name = info_name + date_to_str(date_start) + "_" + date_to_str(date_end) + "_STATS"
+    directory_path = "./Data/" + location_name + "/" + product_type + "/Statistics/"
+    with open(directory_path + file_name + ".json", 'w') as outfile:
+        json.dump(stats, outfile)
+
+
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#              PARAMETERS DEFINITION
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 location_names = ["Bering Strait", "Sabetta Port"]
 product_types = ["CO", "NO2", "CH4"]
@@ -201,29 +298,15 @@ with open(directory_path + "2020.json") as json_file:
     data_2020 = json.load(json_file)
 with open(directory_path + "2021.json") as json_file:
     data_2021 = json.load(json_file)
-with open(directory_path + "info.txt") as json_file:
-    info = json.load(json_file)
 
-data = dict(data_2019["data"])
-data.update(data_2020["data"])
-data.update(data_2021["data"])
+data_set = dict(data_2019["data"])
+data_set.update(data_2020["data"])
+data_set.update(data_2021["data"])
 
+date = datetime.datetime.now()
+date_start = date.replace(year=2021, month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+date_end = date.replace(year=2021, month=9, day=1, hour=0, minute=0, second=0, microsecond=0)
 
-# DAYS
-
-stats = {}
-keys = list(data.keys())
-for i in range(len(keys)):
-    if i != 0:
-        stat = get_image_stats_and_variation(data[keys[i]], data[keys[i-1]])
-        #print("current: " + keys[i] + " 8====D prec: " + keys[i-1])
-    else:
-        stat = get_image_stats(data[keys[i]])
-    stats[keys[i]] = stat
-    if i % 100 == 0: print(i)
-
-# MONTH
+save_period_stats(data_set, date_start, date_end, location_name, product_type)
 
 
-with open(directory_path + 'days.json', 'w') as outfile:
-    json.dump(stats, outfile)
