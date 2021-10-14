90978c6c4bbbc1708d1887a7b0e4e0689a692d63
Pederzh <luigipederzani@gmail.com>
2021-10-14 22:36:09 +0200

    Fixed: Now wind matrix per day is working + print the JSON to file (too huge)

--- wind_formatter.py
+++ wind_formatter.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
 import datetime
-
+import json
 
 # def calculateAngle(value_U, value_V):
 #         return 180 + (
 #                 180 / np.pi * np.arctan2(value_U / np.sqrt(np.power(value_U, 2) + np.power(value_V, 2)),
 #                                          value_V / np.sqrt(np.power(value_U, 2) + np.power(value_V, 2))))
-
 
 def calculateAngle(value_U, value_V):
     return 180 / np.pi * np.arctan2(value_U, value_V)
@@ -20,7 +19,7 @@ def calculateIntensity(value_U, value_V):
 location_names = ["Bering Strait", "Sabetta Port"]
 location_name = location_names[1]
 components = ["u", "v"]  # x, y
-locNum = 185
+locNum = 184
 times = [5, 6, 7, 8]
 lenTimes = len(times)
 pixelLen = 100
@@ -59,25 +58,25 @@ print(df)
 # Iterating over two columns, use `zip`
 date = datetime.datetime.now()
 date_start = date.replace(year=2019, month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
-date_end = date.replace(year=2021, month=9, day=1, hour=0, minute=0, second=0, microsecond=0)
+date_end = date.replace(year=2021, month=10, day=7, hour=0, minute=0, second=0, microsecond=0)
 
 wind = {}
 windMatrix = np.full((100, 100), {})
+timeIndex = 0
 for i, angle, intensity in zip(df.index, df['angle'], df['intensity']):
+
     if i % locNum == 0:
+        if i != 0:
+            wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")] = wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")].tolist() #to print on the file the matrixes TO FIX
+
+        if timeIndex == lenTimes:
+            date_start += datetime.timedelta(days=1)
+            timeIndex = 0
+
         wind[date_start.strftime("%Y-%m-%d")] = {}
-        if i % locNum == 0:
-            date_start = date_start.replace(hour=times[0])
-            wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")] = np.copy(windMatrix)
-        if i % (locNum * 2) == 0:
-            date_start = date_start.replace(hour=times[1])
-            wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")] = np.copy(windMatrix)
-        if i % (locNum * 3) == 0:
-            date_start = date_start.replace(hour=times[2])
-            wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")] = np.copy(windMatrix)
-        if i % (locNum * 4) == 0:
-            date_start = date_start.replace(hour=times[3])
-            wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")] = np.copy(windMatrix)
+        date_start = date_start.replace(hour=times[timeIndex])
+        wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")] = np.copy(windMatrix)
+        timeIndex += 1
 
     subIndex = i % locNum
     x = pixelLen / lenX #4,6
@@ -88,7 +87,6 @@ for i, angle, intensity in zip(df.index, df['angle'], df['intensity']):
     startY = round(y * int(subIndex / lenX))
     endY = round(y * int((subIndex / lenX + 1)))
 
-    print(subIndex, startX, endX, startY, endY)
     val = {
         "angle": angle,
         "intensity": intensity
@@ -96,10 +94,12 @@ for i, angle, intensity in zip(df.index, df['angle'], df['intensity']):
     subMatrix = np.full((endX - startX, endY - startY), val)
 
     wind[date_start.strftime("%Y-%m-%d")][date_start.strftime("%H:%M")][startX:endX, startY:endY] = np.copy(subMatrix)
-    print(i != 0 and i % (locNum * lenTimes) == 0)
-    if i != 0 and i % (locNum * lenTimes) == 0:
-        date_start += datetime.timedelta(days=1)
-        print(date_start)
+
 
 print(wind)
+info_name_write = "wind-matrix_" + location_name.strip()
+file_name_write = info_name_write + date_start.strftime("%Y-%m-%d") + "_" + date_end.strftime("%Y-%m-%d") + "-500_600_700_800"
+directory_path_write = "./data/" + location_name + "/wind/"
+with open(directory_path_write + file_name_write + ".json", 'w') as outfile:
+    json.dump(wind, outfile)
 # result = [generateMatrix(x) for x in df['angle']]
