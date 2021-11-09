70cbdf1e49ee426ab24b79ab82b83ea66558d657
Pederzh <luigipederzani@gmail.com>
2021-11-09 15:46:50 +0100

    Test image api response

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from flask_restful import Api, Resource, reqparse, request
 import pickle
 import numpy as np
@@ -15,9 +15,13 @@ api = Api(app)
 # Define how the api will respond to the post requests
 class TropomiPredictor(Resource):
     def post(self):
+
         print(request.is_json)
         content = request.get_json()
+        
+
         content["status"] = "success"
+        #return send_file('month_real_value-map.png', mimetype='image/png')
         return jsonify(content) # serialize it again so that they will be returned back to the application in a proper format.
 
 
