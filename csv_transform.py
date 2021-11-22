7b1473cd075a5d14ba9555b00874b7c41800c696
Fro <Frao.fiori1997@gmail.com>
2021-11-22 19:19:45 +0100

    Auto stash before rebase of "refs/heads/feat/restApi"
    deleting

--- csv_transform.py
+++
@@ -1,8 +0,0 @@
-location_names = ["Bering Strait", "Sabetta Port"]
-location_name = location_names[1]
-components = ["u", "v"] # x, y
-component = components[1]
-
-with open("Data/" + location_name + "/wind/wind-20190101_20211007-10m_" + component + "-500-OLD.csv", 'r') as f_in, open("data/" + location_name + "/wind/wind-20190101_20211007-10m_" + component + "-500.csv", 'w') as f_out:
-    [f_out.write(','.join(line.split()) + '\n') for line in f_in]
-    f_out.write(next(f_in))
